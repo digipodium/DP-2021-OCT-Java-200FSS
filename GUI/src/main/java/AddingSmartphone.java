60359c1224854aed9d3831a547e5e05d687c34c3
m-triple-m <triplem656@gmail.com>
2022-01-16 20:08:41 +0530

    ..

--- GUI/src/main/java/AddingSmartphone.java
+++ GUI/src/main/java/AddingSmartphone.java
@@ -1,2 +1,5 @@
-package PACKAGE_NAME;public class AddingSmartphone {
+import javax.swing.*;
+
+public class AddingSmartphone {
+    private JButton button1;
 }
--- GUI/src/main/java/UsingFrames.java
+++ GUI/src/main/java/UsingFrames.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class UsingFrames {
+import javax.swing.JFrame;
+import java.awt.*;
+
+public class UsingFrames {
+
+    public static void main(String[] args) {
+        JFrame myframe = new JFrame("My First Frame");
+        myframe.setSize(800, 600);
+        myframe.setVisible(true);
+        myframe.getContentPane().setBackground(Color.cyan);
+        myframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
 }
